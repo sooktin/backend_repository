6b4e31c6e284ff9bdcb4a14b59428efb5b856fa9
hjin0733 <hjin0733h@naver.com>
2024-08-27 19:10:12 +0900

    0827 CRUD - 혜진

--- src/main/java/com/sooktin/backend/controller/BoardController.java
+++ src/main/java/com/sooktin/backend/controller/BoardController.java
@@ -9,4 +9,6 @@ public class BoardController {
      * 클라이언트(우리는 리액트)에게 전달합니다.
      *  controller에서 @GetMapping() { } 등등이 쓰임
      **/
+
+    
 }
--- src/main/java/com/sooktin/backend/domain/Community.java
+++ src/main/java/com/sooktin/backend/domain/Community.java
@@ -1,20 +1,20 @@
 package com.sooktin.backend.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Board {
+public class Community {
     //순수 데이터
     //게시글 조건) Primary Key인 노트ID를 추가하세요. 노트ID는 Long값입니다.
     //내용은 300자 이하이며 not null 제한/좋아요는 null값 허용/생성일시와 수정일시는 timestamp형식
     //게시글 생성/수정/삭제/갖고오기(read)하기.
     //데이터의 속성은 내용 좋아요 생성일시,수정일시가 있음 우선 내용,좋아요만!(가능하다면 이미지도)
-    @NotEmpty
-
+    @Id
 
     @NotEmpty
     private String title;
