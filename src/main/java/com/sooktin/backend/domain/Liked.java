2c3441485c23a70fb21ffb007cac6a25b430a390
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2024-11-24 10:49:02 +0900

    chore: 환경설정 일부 수정

--- src/main/java/com/sooktin/backend/domain/Liked.java
+++ src/main/java/com/sooktin/backend/domain/Liked.java
@@ -24,7 +24,7 @@ public class Liked {
     //좋아요가 게시물에 속하는지 댓글에 속하는지 명확히 구분해야 하기 때문임
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "댓글 id", nullable = true)
-    private Comments comment_id;
+    private Comment comment_id;
 
     //노트 id
     @ManyToOne(fetch = FetchType.LAZY) // 게시물 외래키 - 다대일
