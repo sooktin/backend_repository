c81996b659f3b6e2235034adb16bd38c2eb5e91d
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2024-11-29 00:01:17 +0900

    feat: ResponseDto 추가

---
+++ src/main/java/com/sooktin/backend/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.sooktin.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto<T> {
+    private int code;        // HTTP 상태 코드
+    private String msg;      // 메시지
+    private T data;          // 실제 데이터
+}
