cd82709d4aac74e08d8a07a85aaf9a0c90933532
chaeminyu <chaemin.u@gmail.com>
2025-01-27 02:29:00 +0900

    refactor : like entity

--- src/main/java/com/sooktin/backend/domain/Liked.java
+++ src/main/java/com/sooktin/backend/domain/Liked.java
@@ -3,7 +3,6 @@ package com.sooktin.backend.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Comments;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -30,17 +29,13 @@ public class Liked {
     @JoinColumn(name = "post_id", nullable = true)
     private Usernote post; // Usernote 엔티티와 연결
 
+    @ManyToOne(fetch = FetchType.LAZY) // 사용자 외래키 - 다대일
+    @JoinColumn(name = "user_id")
+    private User user; // User 엔티티와 연결
+
     //좋아요 생성일시
     @CreationTimestamp
     @Column(name = "생성일자", nullable = false)
     private LocalDateTime created_at; // 생성일시
-
-
-    @ManyToOne(fetch = FetchType.LAZY) // 사용자 외래키 - 다대일
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user; // User 엔티티와 연결
-
-    @Column(name = "회원ID",length = 300, nullable = false)
-    private Long userId; // 내용
 
 }
