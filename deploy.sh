701d6048247d1a7150f970d8bdff6a947cee8493
Chaemin Yu <chaemin.u@gmail.com>
2025-02-16 16:35:48 +0900

    test: deploy.sh for nginx update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Docker 컨테이너와 볼륨 재시작
+# 파일 상태 확인
+echo "현재 경로: $(pwd)"
+echo "확인 중... nginx-default.conf:"
+ls -l nginx-default.conf
+echo "nginx-default.conf 내용:"
+cat nginx-default.conf
+
+# 기존 컨테이너와 볼륨 정리
 docker compose down -v
-docker compose build
+docker volume prune -f
+
+# 다시 시작
+docker compose build --no-cache
 docker compose up -d
