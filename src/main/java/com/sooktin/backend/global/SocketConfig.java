5f0ca47c19f2490bc979f148a330a5d6612d9dde
youkm1 <youkm0806@sookmyung.ac.kr>
2025-05-03 22:06:17 +0900

    feat: socket config

--- src/main/java/com/sooktin/backend/global/SocketConfig.java
+++ src/main/java/com/sooktin/backend/global/SocketConfig.java
@@ -1,4 +1,19 @@
 package com.sooktin.backend.global;
 
-public class SocketConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class SocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler(), "/chat").setAllowedOrigins("*");
+    }
+    @Bean
+    public ChatWebSocketHandler chatWebSocketHandler() {
+        return new ChatWebSocketHandler();
+    }
 }
